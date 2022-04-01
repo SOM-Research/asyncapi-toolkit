2d5745eb4f8c6f73e01c04674afd9218f02f2e4f
Abel Gómez <agomezlla@uoc.edu>
2022-04-01 16:35:35 +0200

    Fix validation behaviour of Project Templates with required variables

--- plugins/io.github.abelgomez.asyncapi.ui/src/io/github/abelgomez/asyncapi/ui/wizard/AsyncApiTemplateNewProjectWizard.java
+++ plugins/io.github.abelgomez.asyncapi.ui/src/io/github/abelgomez/asyncapi/ui/wizard/AsyncApiTemplateNewProjectWizard.java
@@ -7,13 +7,13 @@ public class AsyncApiTemplateNewProjectWizard extends TemplateNewProjectWizard {
 
 	@Override
 	public boolean canFinish() {
-		if (templatePage.getSelectedTemplate() instanceof EcoreAsyncApiProject) {
-			EcoreAsyncApiProject selectedTemplate = (EcoreAsyncApiProject) templatePage.getSelectedTemplate();
-			IStatus status = selectedTemplate.validate();
-			if (status != null && !status.isOK()) { 
+		AbstractAsyncApiProjectTemplate selectedTemplate = (AbstractAsyncApiProjectTemplate) templatePage.getSelectedTemplate();
+		IStatus status = selectedTemplate.validate();
+		if (status != null && !status.isOK()) {
+			if (templatePage == null) {
 				templatePage.setErrorMessage(status.getMessage());
-				return false;
 			}
+			return false;
 		}
 		templatePage.setErrorMessage(null);
 		return super.canFinish() && (templateParameterPage == null ? true : templateParameterPage.isPageComplete());
