70c887d7e134f3fc4c478efde5f13a058c8b1f1a
Abel Gómez <agomezlla@uoc.edu>
2022-04-05 21:03:49 +0200

    Add m2 nature to internal projects (will allow using VS code as dev env)
    Also include the AsyncApiSemanticSequence.java file, which gets
    regenerated over and over again with different comments

--- plugins/io.github.abelgomez.asyncapi/src-gen/io/github/abelgomez/asyncapi/serializer/AsyncApiSemanticSequencer.java
+++ plugins/io.github.abelgomez.asyncapi/src-gen/io/github/abelgomez/asyncapi/serializer/AsyncApiSemanticSequencer.java
@@ -366,8 +366,8 @@ public class AsyncApiSemanticSequencer extends AbstractDelegatingSemanticSequenc
 	 * Constraint:
 	 *     (
 	 *         (window=AnyString | windowUnit=WindowUnit | aggregationFunction=AnyString | atomicMetric=AbstractQoSMetric)* 
-	 *         unit=QoSMetricUnit? 
-	 *         ((description=AnyString | dataType=QoSMetricType)? unit=QoSMetricUnit?)*
+	 *         dataType=QoSMetricType? 
+	 *         ((description=AnyString | unit=QoSMetricUnit)? dataType=QoSMetricType?)*
 	 *     )
 	 * </pre>
 	 */
@@ -736,8 +736,8 @@ public class AsyncApiSemanticSequencer extends AbstractDelegatingSemanticSequenc
 	 *             default=PrimitiveValue | 
 	 *             items=AbstractSchema
 	 *         )? 
-	 *         (required+=AnyString required+=AnyString*)? 
 	 *         (enum+=PrimitiveValue enum+=PrimitiveValue*)? 
+	 *         (required+=AnyString required+=AnyString*)? 
 	 *         (properties+=NamedSchema properties+=NamedSchema*)?
 	 *     )+
 	 * </pre>
