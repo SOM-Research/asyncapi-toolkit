4b9976e0e1296b7773959773444523e86531ae99
Abel Gómez <agomezlla@uoc.edu>
2022-05-20 15:40:54 +0200

    Use default height

--- plugins/io.github.abelgomez.asyncapi.ui/src/io/github/abelgomez/asyncapi/ui/wizard/AsyncApiTemplateNewProjectWizard.java
+++ plugins/io.github.abelgomez.asyncapi.ui/src/io/github/abelgomez/asyncapi/ui/wizard/AsyncApiTemplateNewProjectWizard.java
@@ -36,7 +36,7 @@ public class AsyncApiTemplateNewProjectWizard extends TemplateNewProjectWizard {
 	@Override
 	public void createPageControls(Composite pageContainer) {
 		super.createPageControls(pageContainer);
-		getShell().setSize(610, 320);
+		getShell().setSize(610, getShell().getSize().y);
 	}
 	
 	@Override
